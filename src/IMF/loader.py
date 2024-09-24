c23458e789580371ec8d5a886a5db9131c2a1780
George Panagopoulos <geo.png1997@gmail.com>
2024-09-24 20:40:41 +0300

    developed unpack_symbol function, finished IMF loader

--- src/IMF/loader.py
+++ src/IMF/loader.py
@@ -14,20 +14,43 @@ class IMFDataLoader:
         if not os.path.exists(self.json_file_path):
             print("No json file path provided")
             sys.exit(1)
-        with open(json_file_path, 'r') as f:
+        with open(self.json_file_path, 'r') as f:
             text = f.read()
         jsondata = json.loads(text.replace('\'', '\"'))
         return jsondata
         
-    def load_data():
-        pass
+    def load_data(self):
+        jsondata = self.load_json()
+        data = pd.DataFrame(columns=self.DB_COLUMNS)
+        for symbol in jsondata.keys():
+            df = self.unpack_symbol(jsondata, symbol)
+            data = pd.concat([data, df], ignore_index=True)
+        return data
+
     def update_forecast_status(self, date=None):
         """ last_historical_date.txt file contains the last date for which we have actual historical recorded data.
             returns 1 or 0 if date is greater than the last historical date in the file.
         """
         return int(date[:4] >= self.last_historical_date[:4])
 
+    def unpack_symbol(self, jsondata, symbol):
+        result =[]
+        data = jsondata[symbol]
+        for area, values in data.items():
+            for date, value in values.items():
+                result.append({'inst_instid': 'IMF', 
+                               'indic_indicid': symbol, 
+                               'area_areaid': area, 
+                               'value': value, 'value_normalized': None, 
+                               'date_from': date, 'date_until': None, 'date_published': None, 'date_updated': None, 
+                               'is_forecast': self.update_forecast_status(date)})
+        return pd.DataFrame(result)
+        
+
+        
 if __name__ == "__main__":
-    ld = OECDDataLoader("data/OECD_data.csv")
+    ld = IMFDataLoader("data/IMF_data.json")
     df = ld.load_data()
-    print(df[df['is_forecast'] != 0].head())
+    print(df[['inst_instid', "indic_indicid", "area_areaid", "date_from", "is_forecast"]].head())
+    print(df.shape)
+    print(df[['inst_instid', "indic_indicid", "area_areaid", "date_from", "is_forecast"]].tail())
