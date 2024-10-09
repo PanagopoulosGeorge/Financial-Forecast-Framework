1eefe23308e5de37841e25a4f17f93650ac2ef31
George Panagopoulos <geo.png1997@gmail.com>
2024-10-09 09:46:11 +0300

    correct

--- install/postgres/3. grant_priviledge.sql
+++ install/postgres/3. grant_priviledge.sql
@@ -1,5 +1,5 @@
 -- Grant permissions to all tables in the public schema
-GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO db3test;
+GRANT SELECT, INSERT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO db2test;
 
 -- Optionally, grant permissions on future tables in the public schema
-ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO db3test;
+ALTER DEFAULT PRIVILEGES IN SCHEMA public GRANT SELECT, INSERT, UPDATE, DELETE ON TABLES TO db2test;
